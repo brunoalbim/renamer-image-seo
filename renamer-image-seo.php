708dd25619c78328b8584d175a0a2a2143866092
brunoalbim <bruno_albim@hotmail.com>
2024-08-26 14:47:42 -0300

    Update renamer-image-seo.php

--- renamer-image-seo.php
+++ renamer-image-seo.php
@@ -1,28 +1,35 @@
 <?php
 /*
 Plugin Name: Renamer Image SEO
-Description: Renomeia as imagens enviadas para incluir o nome do site e otimiza o texto alternativo.
-Version: 0.1.3
+Description: Renomeia as imagens enviadas para incluir o nome e a descrição do site, além de otimizar o texto alternativo.
+Version: 0.1.6
 Author: Bruno A
 */
 
 function custom_image_renamer($file) {
     $site_name = get_bloginfo('name');
+    $site_description = get_bloginfo('description');
+    
+    // Sanitiza e cria slugs para o título e a descrição do site
     $site_name_slug = sanitize_title($site_name);
+    $site_description_slug = sanitize_title($site_description);
     
     $info = pathinfo($file['name']);
     $ext = empty($info['extension']) ? '' : '.' . $info['extension'];
     $name = basename($file['name'], $ext);
 
-    // Armazena o nome original do arquivo em uma variável temporária para uso posterior
+    // Armazena o nome original do arquivo (sem formatação)
     $original_name = $name;
 
-    // Remove caracteres não otimizados para SEO, exceto números, convertendo para minúsculas e substituindo espaços por hífens
+    // Remove acentos e caracteres especiais do nome do arquivo para otimização SEO
+    $name = remove_accents($name);
+
+    // Remove caracteres não otimizados para SEO e converte para minúsculas
     $name = strtolower(preg_replace('/[^a-zA-Z0-9\s]/', '', $name));
     $name = sanitize_title($name);
 
-    // Adiciona o nome do site ao final do nome do arquivo
-    $new_name = $name . '-' . $site_name_slug . $ext;
+    // Adiciona o nome e a descrição do site ao final do nome do arquivo
+    $new_name = $name . '-' . $site_name_slug . '-' . $site_description_slug . $ext;
     $file['name'] = $new_name;
 
     // Armazena o nome original na sessão para uso na função custom_image_alt_text
@@ -44,9 +51,10 @@ function custom_image_alt_text($post_ID) {
     $original_name = $_SESSION['original_name'];
     unset($_SESSION['original_name']); // Limpa a variável de sessão após o uso
 
-    // Adiciona o nome do site ao alt text
+    // Adiciona o nome do site e a descrição ao alt text
     $site_name = get_bloginfo('name');
-    $alt_text = $original_name . ' - ' . $site_name;
+    $site_description = get_bloginfo('description');
+    $alt_text = $original_name . ' - ' . $site_name . ' - ' . $site_description;
     
     // Atualiza o alt text da imagem
     update_post_meta($post_ID, '_wp_attachment_image_alt', $alt_text);
