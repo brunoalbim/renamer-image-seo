f1cbaca51e1ca09f6927e2a9f988be0b4f03c4a7
Bruno Devx <contato@brunoalbim.com.br>
2025-09-30 11:25:23 -0300

    Update renamer-image-seo.php

--- renamer-image-seo.php
+++ renamer-image-seo.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WP Renamer Image SEO
 Description: Renomeia as imagens enviadas para incluir o nome e a descrição do site, além de otimizar o texto alternativo.
-Version: 0.1.10
+Version: 0.1.11
 Author: Bruno A
 */
 
@@ -13,11 +13,9 @@ function custom_image_renamer($file) {
     }
 
     $site_name = get_bloginfo('name');
-    $site_description = get_bloginfo('description');
-    
-    // Sanitiza e cria slugs para o título e a descrição do site
+
+    // Sanitiza e cria slugs para o título do site
     $site_name_slug = sanitize_title($site_name);
-    $site_description_slug = sanitize_title($site_description);
     
     $info = pathinfo($file['name']);
     $ext = empty($info['extension']) ? '' : '.' . $info['extension'];
@@ -35,7 +33,7 @@ function custom_image_renamer($file) {
     $name = sanitize_title($name);
 
     // Adiciona o nome e a descrição do site ao final do nome do arquivo
-    $new_name = $name . '-' . $site_name_slug . '-' . $site_description_slug . $ext;
+    $new_name = $name . '-' . $site_name_slug . $ext;
     $file['name'] = $new_name;
 
     // Armazena o nome original na sessão para uso na função custom_image_alt_text
@@ -57,10 +55,9 @@ function custom_image_alt_text($post_ID) {
     $original_name = $_SESSION['original_name'];
     unset($_SESSION['original_name']); // Limpa a variável de sessão após o uso
 
-    // Adiciona o nome do site e a descrição ao alt text
+    // Adiciona o nome do site ao alt text
     $site_name = get_bloginfo('name');
-    $site_description = get_bloginfo('description');
-    $alt_text = ucfirst(str_replace('-', ' ', $original_name)) . ' - ' . $site_name . ' - ' . $site_description;
+    $alt_text = ucfirst(str_replace('-', ' ', $original_name)) . ' - ' . $site_name;
     
     // Atualiza o alt text da imagem
     update_post_meta($post_ID, '_wp_attachment_image_alt', $alt_text);
